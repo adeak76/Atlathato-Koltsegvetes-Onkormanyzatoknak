9cce679148bb86623eef82a79997743f614fecb3
adeak76 <deak.attila@vegacom.hu>
2026-03-12 10:21:44 +0100

    Update BudgetExplanation.jsx

--- src/components/BudgetExplanation.jsx
+++ src/components/BudgetExplanation.jsx
@@ -87,8 +87,8 @@ export default function BudgetExplanation() {
                 <p style={{ fontSize: '0.9rem', color: 'var(--text-muted)', lineHeight: 1.6, margin: 0 }}>
                     Fontos fogalmak! Az adott év elején elfogadott tervet hívjuk <strong>Költségvetési Tervezetnek / Eredeti Előirányzatnak</strong> (pl. a {config.alapEv}. évi adatok).
                     Azonban év közben történhetnek változások (pl. az önkormányzat nyer egy 10 milliós pályázatot, amit bele kell írni).
-                    Az év végén, a ténylegesen elköltött összegeket tartalmazó végleges listát hívjuk <strong>Zárszámadásnak (Tényadatoknak)</strong>. Az átláthatóság érdekében {config.alapEv - 1}-ig visszamenőleg már a valós Tényadatokat láthatja a görbéken, 
-                    valamint a reálisabb pénzügyi helyzetértékelés érdekében a Működési Bevételek között a <i>Pénzügyi Műveletek Bevételei (pl. kamatbevételek)</i> nem szerepelnek!
+                    Az év végén, a ténylegesen elköltött összegeket tartalmazó végleges listát hívjuk <strong>Zárszámadásnak (Tényadatoknak)</strong>. Az átláthatóság érdekében {config.alapEv - 1}-ig visszamenőleg már a valós Tényadatokat láthatja a görbéken. 
+                    A reálisabb pénzügyi helyzetértékelés érdekében a <strong>Közös Önkormányzati Hivatal költségei</strong> a Személyi Jellegű Kiadások között szerepelnek, valamint a Működési Bevételek között a <i>Pénzügyi Műveletek Bevételei (pl. kamatbevételek)</i> nem szerepelnek!
                 </p>
             </div>
 
