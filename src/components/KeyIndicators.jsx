0cd0044b6eae6849469872e04dbdaddf93015941
adeak76 <deak.attila@vegacom.hu>
2026-03-13 20:51:59 +0100

    Update KeyIndicators.jsx

--- src/components/KeyIndicators.jsx
+++ src/components/KeyIndicators.jsx
@@ -162,8 +162,8 @@ export default function KeyIndicators({ data, historicalData }) {
                 <div className="glass-panel" style={{ padding: '1.5rem', height: '400px', display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
                     <div className="responsive-header">
                         <div>
-                            <h4 style={{ color: 'var(--text-muted)' }}>Helyi Adók alakulása és arányai (millió Ft)</h4>
-                            {data.yoy && formatYoY(data.yoy.taxTotal)}
+                            <h4 style={{ color: 'var(--text-muted)' }}>Helyi Adók alakulása és arányai (millió Ft)</h4></br>
+                            Változás az előző évhez képest {data.yoy && formatYoY(data.yoy.taxTotal)}
                             {/* Ide írhatsz települési adópolitikai megjegyzést, ha van ilyen */}
                         </div>
                         <div className="responsive-stats">
